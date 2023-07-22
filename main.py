4b38dfb7f37f3d375ea032c36fdccdae5ff282e7
FunyKaly <136472156+funykaly@users.noreply.github.com>
2023-07-22 18:54:00 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import pytz
+import asyncio
+from datetime import datetime
+from telethon import TelegramClient
+from telethon.tl.functions.account import UpdateProfileRequest
+
+
+
+api_id = <api_id>
+api_hash = "<api_hash>"
+
+time_texts = {
+    1: "🕐", 2: "🕑", 3: "🕒", 4: "🕓", 5: "🕔", 6: "🕕", 7: "🕖", 8: "🕗", 9: "🕘",
+    10: "🕙", 11: "🕚", 12: "🕛", 13: "🕐", 14: "🕑", 15: "🕒", 16: "🕓", 17: "🕔",
+    18: "🕕", 19: "🕖", 20: "🕗", 21: "🕘", 22: "🕙", 23: "🕚", 0: "🕛"
+}
+
+timezone = pytz.timezone('<your_timezone>')
+
+async def update_profile():
+    async with TelegramClient("<app_title>", api_id, api_hash) as client:
+        while True:
+            try:
+                now = datetime.now(timezone)
+                hour = now.hour
+                about_text = f"You will see this at {now.strftime('%H:%M')} {time_texts.get(hour, '')}"
+
+                await client(UpdateProfileRequest(about=about_text))
+                await asyncio.sleep(60 - now.second)
+            except Exception as e:
+                print(f"Error: {e}")
+            finally:
+                pass
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(update_profile())
+    except KeyboardInterrupt:
+        print("Execution interrupted.")
